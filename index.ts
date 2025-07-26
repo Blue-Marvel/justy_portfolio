3a90d71c05a85ea594e8b94f89536d4ba04c2f1c
Blue-Marvel <marvellousogbo0@gmail.com>
2025-07-26 22:14:31 +0100

    index.ts

---
+++ index.ts
@@ -0,0 +1,48 @@
+// console.log("Hello World");
+
+let myName = "Justy";
+
+const takeAwayContainer = "Rice";
+
+console.log(takeAwayContainer, "First Log");
+
+myName = "Chinecherem";
+
+// takeAwayContainer = "Beans";
+
+//The below lines will print the value of myName 
+// and takeAwayContainer
+// console.log(myName, "Second Log");
+// console.log(takeAwayContainer, "Third Log");
+
+//Data Types new TOPIC
+
+
+//Primitive Data Types
+//String
+
+//Strings are sequences of characters that can be used to represent text.
+// * They are always enclosed inside double or single quotes.
+//Example
+"Hello World"
+'Hello World 520752558***(((&*(&o87976😘@@#$%^&*'
+
+//If there's a single quotation mark inside the string, you need to escape it with a backslash (\) or
+// you can enclose the string with double quotes.
+console.log("She's Mine")
+console.log('She\'s Mine');
+
+//This specify the type of data in the variable
+let greeting: string;
+
+greeting = "Hello World";
+
+
+//Boolean
+//Null
+//Undefined
+
+//Reference Data Types
+//Array
+//Object
+

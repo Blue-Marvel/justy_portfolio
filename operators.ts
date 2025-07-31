00f8caa4694eca7e0cdaa68d716fdff57403695e
Blue-Marvel <marvellousogbo0@gmail.com>
2025-07-31 08:08:31 +0100

    Arithmetic operators

---
+++ operators.ts
@@ -0,0 +1,24 @@
+//Arithmetic Operators.
+//Addition +
+let sum = 5 + 3;
+console.log("Sum of 5 and 3 is: " + sum + "...");
+//Concatenation of strings: 
+// this is also known as joining two or 
+// more string together
+//  to form one(can join any other data types with strings too).
+
+//Subtraction -
+let difference = 5 - 3;
+console.log("Difference of 5 and 3 is: " + difference + "...");
+//Multiplication *
+let product = 5 * 3;
+console.log("Product of 5 and 3 is: " + product + "...");
+//Division /
+let quotient = 9 / 3;
+console.log("Quotient of 9 and 3 is: " + quotient + "...");
+//Remainder(modulus) %
+let remainder = 10 % 3;
+console.log("Remainder of 10 and 3 is: " + remainder + "...");
+//Exponentiation **
+let exponent = 2 ** 3;
+console.log("Exponent of 2 and 3 is: " + exponent + "...");
